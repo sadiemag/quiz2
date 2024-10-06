76ef384d59cb70f38e2fe0d8a6d1f02852efa58d
sadiemag <sadmag0505@gmail.com>
2024-10-06 13:45:06 +0100

    Add files via upload

---
+++ quiz2.R
@@ -0,0 +1,21 @@
+
+
+
+library(tidyverse)
+us_rent_income %>% head(n = 10)
+help('us_rent_income')
+
+a <- c(24476,32940,27517,23789,29454)
+mean(a)
+
+b <- c(747,1200,972,709,1358)
+mean(b)
+
+
+library(tidyverse)
+household
+
+library(tidyverse)
+us_rent_income %>% head(n = 10)
+help('us_rent_income')
+group_by(GEOID, NAME, variable = income, estimate)
